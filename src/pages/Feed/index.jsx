f0336c1050176e7a14d08a9d761f604b39630042
Jennifer Alarcon Serna <jenniferalarconserna@gmail.com>
2023-10-24 17:31:12 -0500

    Posts component and PostCard component styles complete

--- src/pages/Feed/index.jsx
+++ src/pages/Feed/index.jsx
@@ -1,13 +1,18 @@
 import Header from '../../components/Header'
 import Status from '../../components/Status'
+import Posts from '../../components/Posts'
 import './styles.sass'
+import PostCard from '../../components/PostCard'
 
 const Feed = () => {
   return (
     <main className='feed'>
       <Header />
       <Status />
-      feed
+      <Posts>
+        <PostCard />
+        <PostCard />
+      </Posts>
     </main>
   )
 }
