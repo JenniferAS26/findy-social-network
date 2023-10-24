b161041e747a357c8fc67ca93c722a734beca744
Jennifer Alarcon Serna <jenniferalarconserna@gmail.com>
2023-10-24 15:37:59 -0500

    Status component styles complete

--- src/pages/Feed/index.jsx
+++ src/pages/Feed/index.jsx
@@ -1,10 +1,12 @@
 import Header from '../../components/Header'
+import Status from '../../components/Status'
 import './styles.sass'
 
 const Feed = () => {
   return (
     <main className='feed'>
       <Header />
+      <Status />
       feed
     </main>
   )
