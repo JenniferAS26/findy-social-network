e7f563174f0f280b87ba632862767dd432870428
Jennifer Alarcon Serna <jenniferalarconserna@gmail.com>
2023-10-25 20:13:01 -0500

    Creation followingService

---
+++ src/services/followingService.js
@@ -0,0 +1,33 @@
+import axios from 'axios'
+import { endpoints } from './endpoints'
+
+const addFollow = async ( body ) => {
+  try {
+    await axios.post(endpoints.following, body)
+  } catch (error) {
+    console.warn(error)
+  }
+}
+
+const getFollowing = async ( id = '' ) => {
+  try {
+    const { data } = await axios.get(endpoints.following, id)
+    return data
+  } catch (error) {
+    console.warn(error)
+  }
+}
+
+const removeFollow = async ( id ) => {
+  try {
+    await axios.delete(endpoints.following, id)
+  } catch (error) {
+    console.warn(error)
+  }
+}
+
+export {
+  addFollow,
+  getFollowing,
+  removeFollow
+}
