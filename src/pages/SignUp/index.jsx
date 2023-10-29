3567eb15211a9cd8bd83f64fbbc7d044c8655c79
chesterlm <loaizamazocristian@gmail.com>
2023-10-29 18:30:27 -0500

    user validation, effect corrected, sweealert implemented in sign up

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -3,6 +3,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import './styles.scss';
 import flechitaIcon from '../../assets/icons/flechita.png';
 import { createUser } from '../../services/userService.js';
+import Swal from 'sweetalert2';
 
 const SignUpForm = () => {
   const [step, setStep] = useState(1);
@@ -11,6 +12,7 @@ const SignUpForm = () => {
     password: '',
     birthdate: '',
     username: '',
+    email: '', 
     phoneNumber: '',
   });
 
@@ -30,15 +32,17 @@ const SignUpForm = () => {
   const isStepValid = () => {
     switch (step) {
       case 1:
-        return formData.name.trim() !== '';
+        return formData.name.trim() !== '' && /^[a-zA-Z\s]*$/.test(formData.name);
       case 2:
-        return formData.password.trim() !== '' && formData.password.length >= 6;
+        return formData.password.trim() !== '' && /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/.test(formData.password);
       case 3:
         return formData.birthdate.trim() !== '';
       case 4:
-        return formData.username.trim() !== '';
+        return formData.username.trim() !== '' && /^[a-zA-Z\s]*$/.test(formData.username);
       case 5:
-        return formData.phoneNumber.trim() !== '';
+        return formData.email.trim() !== '' && /\S+@\S+\.\S+/.test(formData.email);
+      case 6:
+        return formData.phoneNumber.trim() !== '' && /^[1-9]*$/.test(formData.phoneNumber);
       default:
         return true;
     }
@@ -50,10 +54,21 @@ const SignUpForm = () => {
     if (isStepValid()) {
       try {
         const newUser = await createUser(formData);
-        console.log(newUser); 
-        navigate('/');
+        console.log(newUser);
+
+        Swal.fire({
+          icon: 'success',
+          title: 'Successful Registration',
+          text: 'Your account has been created successfully!',
+          confirmButtonColor: '#FF7674',
+          customClass: {
+            confirmButton: 'custom-button-width'
+          }
+        }).then(() => {
+          navigate('/sign-in');
+        });
       } catch (error) {
-        console.error('Error al crear el usuario:', error);
+        console.error('Error creating the user:', error);
       }
     }
   };
@@ -71,96 +86,108 @@ const SignUpForm = () => {
         )}
         {step === 1 && (
           <div>
-            <h2>¿Cómo te llamas?</h2>
+            <h2>What is your name?</h2>
             <input
               type="text"
               name="name"
-              placeholder="Nombre completo"
+              placeholder="Full name"
               value={formData.name}
               onChange={handleChange}
               required
             />
             <button onClick={handleNextStep} disabled={!isStepValid()}>
-              Siguiente
+              Next
             </button>
           </div>
         )}
         {step === 2 && (
           <div>
-            <h2>Crea una contraseña</h2>
+            <h2>Create a password</h2>
             <input
               type="password"
               name="password"
-              placeholder="Contraseña"
+              placeholder="Password"
               value={formData.password}
               onChange={handleChange}
               required
             />
-            <p>Crea una contraseña de al menos 6 letras o números. Debe ser algo difícil de adivinar.</p>
+            <p>Create a password with at least 6 characters, including an uppercase letter, a lowercase letter, a number, and a special character !#$%</p>
             <button onClick={handleNextStep} disabled={!isStepValid()}>
-              Siguiente
+              Next
             </button>
           </div>
         )}
         {step === 3 && (
           <div>
-            <h2>¿Cuándo naciste?</h2>
+            <h2>When were you born?</h2>
             <input
               type="date"
               name="birthdate"
-              placeholder="Fecha de nacimiento"
+              placeholder="Date of birth"
               value={formData.birthdate}
               onChange={handleChange}
               required
             />
-            <p>
-              Indica tu fecha de nacimiento. Aunque esta cuenta sea para un negocio, una mascota, etc., nadie la verá
-              a menos que decidas compartirla.
-            </p>
+            <p>Specify your date of birth. Even if this account is for a business, a pet, etc., no one will see it unless you decide to share it.</p>
             <button onClick={handleNextStep} disabled={!isStepValid()}>
-              Siguiente
+              Next
             </button>
           </div>
         )}
         {step === 4 && (
           <div>
-            <h2>Crea tu nombre de usuario</h2>
+            <h2>Create your username</h2>
             <input
               type="text"
               name="username"
-              placeholder="Nombre de usuario"
+              placeholder="Username"
               value={formData.username}
               onChange={handleChange}
               required
             />
-            <p>Agrega un nombre de usuario; puedes cambiarlo cuando quieras.</p>
+            <p>Add a username; you can change it anytime you want.</p>
             <button onClick={handleNextStep} disabled={!isStepValid()}>
-              Siguiente
+              Next
             </button>
           </div>
         )}
         {step === 5 && (
           <div>
-            <h2>¿Cuál es tu número de celular?</h2>
+            <h2>Enter your email</h2>
             <input
-              type="tel"
-              name="phoneNumber"
-              placeholder="Número de celular"
-              value={formData.phoneNumber}
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={formData.email}
               onChange={handleChange}
               required
             />
-            <p>
-              Es posible que te enviemos notificaciones por SMS con fines de seguridad e inicio de sesión.
-            </p>
+            <button onClick={handleNextStep} disabled={!isStepValid()}>
+              Next
+            </button>
+          </div>
+        )}
+        {step === 6 && (
+          <div>
+            <h2>What is your mobile number?</h2>
+            <input
+              type="tel"
+              name="phoneNumber"
+              placeholder="Mobile number"
+              value={formData.phoneNumber}
+              onChange={handleChange}
+              pattern="[1-9]*"
+              required
+            />
+            <p>We may send you SMS notifications for security and login purposes.</p>
             <button onClick={handleSignUp} disabled={!isStepValid()}>
-              Registrarse
+              Sign Up
             </button>
           </div>
         )}
       </div>
       <p className="sign-in-link">
-        ¿Ya tienes cuenta? <Link className="link" to="/sign-in">Inicia Sesión</Link>
+        Already have an account? <Link className="link" to="/sign-in">Sign In</Link>
       </p>
     </div>
   );
