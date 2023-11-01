27f7e6b00d32fd8f1b295f2034e84609c5358e6c
Jennifer Alarcon Serna <jenniferalarconserna@gmail.com>
2023-10-31 19:03:02 -0500

    PostDetail with dynamic route

--- src/pages/PostDetail/index.jsx
+++ src/pages/PostDetail/index.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import back from '../../assets/icons/back-white.svg'
 import menu from '../../assets/icons/menu-white.svg'
 import postPicture from '../../assets/images/post-picture.svg'
@@ -7,8 +7,23 @@ import sendComment from '../../assets/icons/send-message.svg'
 
 import './styles.sass'
 import FloatingCard from '../../components/FloatingCard'
+import { useCallback, useEffect, useState } from 'react'
+import { getPostByParams } from '../../services/postsService'
 
 const PostDetail = () => {
+  const [postDetails, setPostDetails] = useState([])
+  const { id } = useParams()
+  console.log(id)
+
+  const getOnePost = useCallback(() => {
+    getPostByParams({ postId: id })
+      .then(response => setPostDetails(response))
+  }, [])
+  
+  useEffect(() => {
+    getOnePost()
+  }, [getOnePost])
+
   return (
     <main className='post-detail'>
       <section className='post-detail__cover-picture'>
@@ -18,12 +33,12 @@ const PostDetail = () => {
           </Link>
           <img src={menu} alt='menu icon' />
         </div>
-        <img className='post-detail__cover-picture--image' src={postPicture} alt='cover picture from feed' />
+        <img className='post-detail__cover-picture--image' src={postDetails[0]?.urlContent} alt='cover picture from feed' />
       </section>
       <section className='post-detail__description'>
-        <p className='post-detail__description--text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed voluptates et reprehenderit consequuntur hic, fugit incidunt nemo facilis in. Quas, aliquid perferendis? Deserunt obcaecati, repellendus amet consectetur dignissimos at maxime. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Culpa, libero. Reiciendis.</p>
+        <p className='post-detail__description--text'>{postDetails[0]?.description}</p>
       </section>
-      <FloatingCard />
+      <FloatingCard postDetails={postDetails} />
       <form className='post-detail__new-comment'>
         <img className='post-detail__new-comment--image' src={profilePicture} alt='profile picture' />
         <div className='post-detail__new-comment--input-wrapper'>
