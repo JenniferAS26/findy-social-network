2f0ba473897d2136e8e9432073c715da482c53e9
kelly saldaña <kellysalb@hotmail.com>
2023-11-07 18:04:19 -0500

    filtrado completo

--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -1,106 +1,78 @@
-import { useState, useEffect, useCallback } from 'react'
-import { useParams } from 'react-router-dom'
-/* import PropTypes from 'prop-types'; */
-import { getFollowingByParams } from '../../services/followingService'
-import './styles.scss';
-import { getPostByParams } from '../../services/postsService';
+import { useState, useEffect, useCallback } from "react";
+import { useParams } from "react-router-dom";
+import { getFollowingByParams } from "../../services/followingService";
+import { getPostByParams } from "../../services/postsService";
+import "./styles.scss";
 
 const Gallery = () => {
-  const [filteredItems, setFilteredItems] = useState([]);
-  const [filter, setFilter] = useState('all'); 
-  const {username} = useParams()
+  const [postFollowing, setPostFollowing] = useState([]);
+  const [filter, setFilter] = useState("image");
+  const { username } = useParams();
   console.log(username);
-  const [postFromUsers, setPostFromUsers] = useState([])
+  const [postFromUsers, setPostFromUsers] = useState([]);
 
-
-  const renderItem = (item) => {
-    switch(item.type) {
-      case 'photo':
-        return <img src={item.url} alt={`Photo ${item.id}`} />;
-      case 'video':
-        return (
-          <video controls>
-              {item.videos.map((video, index) => (
-                <source key={index} src={video.url} type="video" />
-            ))}
-             Your browser does not support the video tag.
-          </video>
-        );
-      case 'album':
-        return (
-          <div className="album">
-            {item.photos.map((photo, index) => (
-              <img key={index} src={photo.url} alt={`Album Photo ${index}`} />
-            ))}
-          </div>
-        );
-      case 'tags':
-        return <div className="tags">{item.tags.join(', ')}</div>;
-   /*  default:
-    return <div>Unsupported item type</div>; */
-    }
-  };
-  
- /*  const filterItems = (items, filter) => {
-    let filtered;
-    if (filter === 'all') {
-      filtered = items;
-    } else {
-      filtered = items.flatMap(item =>
-        item.posts.filter(post => post.type === filter)
-      );
-    }
-    setFilteredItems(filtered);
-  }; */
+  const filteredPosts = postFollowing?.posts?.filter(post => post.type === filter)
 
   const getPostsFromUsers = useCallback(() => {
-    getPostByParams({ username })
-      .then(response => setPostFromUsers(response))
+    getPostByParams({ username }).then((response) =>
+      setPostFromUsers(response)
+    );
+  }, []);
+
+  const getPostFromFollowing = useCallback(() => {
+    getFollowingByParams({ username }).then((response) => setPostFollowing(response[0]));
   }, [])
 
-  useEffect(() => {
-    getPostsFromUsers()
-  }, [getPostsFromUsers])
 
   useEffect(() => {
-    getFollowingByParams({username})
-      .then(response => {
-        console.log(response);
-        setFilteredItems(response)
-      })
-  
-  }, [filter]);
+    getPostsFromUsers();
+  }, [getPostsFromUsers]);
 
- /*  useEffect(() => {
-    filterItems(items, filter); // Filtra items cada vez que el filtro cambie.
-  }, [filter, items]);
- */
+  useEffect(() => {
+    getPostFromFollowing()
+  }, [getPostFromFollowing]);
+
+
   
- 
   return (
     <section>
       <div className="Container-Gallery">
         <div className="Container-Gallery_header">
-          <button className='Photos' onClick={() => setFilter('photos')}>Photos</button>
-          <button className='Videos' onClick={() => setFilter('videos')}>Videos</button>
-          <button className='album' onClick={() => setFilter('album')}>Album</button>
-          <button className='tag' onClick={() => setFilter('tags')}>Tags</button>
+          <button 
+            className="Photos" 
+            onClick={() => {
+              setFilter("image")
+              console.log(filter)
+            }}
+          >
+            Photos
+          </button>
+          <button className="Videos" onClick={() => setFilter("video")}>
+            Videos
+          </button>
+          <button className="album" onClick={() => setFilter("album")}>
+            Album
+          </button>
+          <button className="tag" onClick={() => setFilter("tags")}>
+            Tags
+          </button>
         </div>
         <div className="Container-Gallery_cards">
-          {/* {filteredItems.map((item, index) => (
-          {/*  {filteredItems.map((item, index) => (
-            <div key={index} className="gallery-item">
-              {renderItem(item, filter === 'photos' ? 'photo' : 'video')}
-            </div>
-          ))} */}
           {
             postFromUsers.length
               ? postFromUsers.map((post, index) => (
-                <img key={index} className='cardImage' src={post.urlContent} alt="" />
-              ))
-              : filteredItems[0]?.posts.map((post, index) => (
-              <img key={index} className='cardImage' src={post.urlContent} alt="" />
-            ))
+                  <img
+                    key={index}
+                    className="cardImage"
+                    src={post.urlContent}
+                    alt=""
+                  />
+                ))
+              : filteredPosts?.map((post, index) => (
+                  filter === 'image'
+                    ? <img key={index} className="cardImage" src={post.urlContent} alt=""/>
+                    : <video key={index} className="cardImage" src={post.urlContent} controls></video>
+                ))
           }
         </div>
       </div>
