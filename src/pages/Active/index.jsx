8c6fdf9b5fa35badc3c1bdfa2cae591428ca0ee2
Mrinconrincon <rinconrinconmonica@gmail.com>
2023-11-07 17:39:45 -0500

    Active-actualización

--- src/pages/Active/index.jsx
+++ src/pages/Active/index.jsx
@@ -14,7 +14,7 @@ import img11 from '../../assets/images/woman11.webp'
 import img12 from '../../assets/images/woman12.jpg'
 import img13 from '../../assets/images/woman13.jpg'
 import camara from '../../assets/icons/camara.png'
-
+import camara2 from '../../assets/icons/camara2.png'
 const Active = () => {
   return ( 
     <div className="active-container">
@@ -56,138 +56,162 @@ const Active = () => {
 
       <div className='columnas'>
         <div className='columna1'>
-          <img src={img13} alt="" />
-          <div className='information'>  
-            <p>Amanda Serna Leon</p>
-            <span> 10 new messages - 5h</span>
-         </div>
+          <div className='wrapper'>
+            <img src={img13} alt="" />
+            <div className='information'>
+              <p>Amanda Serna Leon</p>
+              <span> 10 new messages - 5h</span>
+            </div>
+          </div>
          <div className='icono'>
-           <img className='camara' src= {camara} alt="" />  
+           <img className='camara' src= {camara2} alt="" />  
          </div>
         </div>
         <div className='columna2'>
-          <img src={img12} alt="" />
-          <div className='information'>  
-            <p>Sofia Olarte</p>
-            <span> 4 new messages - 18h</span>
-         </div>
+          <div className='wrapper'>
+            <img src={img12} alt="" />
+            <div className='information'>
+              <p>Sofia Olarte</p>
+              <span> 4 new messages - 18h</span>
+                     </div>
+          </div>
          <div className='icono'>
-           <img className='camara' src= {camara} alt="" />  
+           <img className='camara' src= {camara2} alt="" />  
          </div>
         </div>
         <div className='columna3'>
-          <img src={img11} alt="" />
-          <div className='information'>  
-            <p>Daniela</p>
-            <span> 14 + new messages 22h</span>
-         </div>
+          <div className='wrapper'>
+            <img src={img11} alt="" />
+            <div className='information'>
+              <p>Daniela</p>
+              <span> 14 + new messages 22h</span>
+            </div>
+          </div>
          <div className='icono'>
-           <img className='camara' src= {camara} alt="" />  
+           <img className='camara' src= {camara2} alt="" />  
          </div>
         </div>
         <div className='columna4'>
-          <img src={img10} alt="" />
-          <div className='information'>  
-            <p>Catalina A- Rodriguez</p>
-            <span> Seen 14h ago</span>
-         </div>
+          <div className='wrapper'>
+            <img src={img10} alt="" />
+            <div className='information'>
+              <p>Catalina A- Rodriguez</p>
+              <span> Seen 14h ago</span>
+            </div>
+          </div>
          <div className='icono'>
-           <img className='camara' src= {camara} alt="" />  
+           <img className='camara' src= {camara2} alt="" />  
          </div>
         </div>
         <div className='columna5'>
-          <img src={img9} alt="" />
-          <div className='information'>  
-            <p>Yesse Acevedo</p>
-            <span>Sent yesterday</span>
-         </div>
+          <div className='wrapper'>
+            <img src={img9} alt="" />
+            <div className='information'>
+              <p>Yesse Acevedo</p>
+              <span>Sent yesterday</span>
+            </div>
+          </div>
          <div className='icono'>
-           <img className='camara' src= {camara} alt="" />  
+           <img className='camara' src= {camara2} alt="" />  
          </div>
         </div>
         <div className='columna6'>
-          <img src={img8} alt="" />
-          <div className='information'>  
-            <p>Sofia Olarte</p>
-            <span> Seen yesterday</span>
-         </div>
+          <div className='wrapper'>
+            <img src={img8} alt="" />
+            <div className='information'>
+              <p>Sofia Olarte</p>
+              <span> Seen yesterday</span>
+            </div>
+          </div>
          <div className='icono'>
-           <img className='camara' src= {camara} alt="" />  
+           <img className='camara' src= {camara2} alt="" />  
          </div>
         </div>
         <div className='columna7'>
-          <img src={img7} alt="" />
-          <div className='information'>  
-            <p>Jeenifer Alarcon</p>
-            <span> 4 new messages 18h</span>
-         </div>
+          <div className='wrapper'>
+            <img src={img7} alt="" />
+            <div className='information'>
+              <p>Jeenifer Alarcon</p>
+              <span> 4 new messages 18h</span>
+            </div>
+          </div>
          <div className='icono'>
-           <img className='camara' src= {camara} alt="" />  
+           <img className='camara' src= {camara2} alt="" />  
          </div>
         </div>
         <div className='columna8'>
-          <img src={img6} alt="" />
-          <div className='information'>  
-            <p>Kelly Saldaña</p>
-            <span> 10+ new 22h</span>
-         </div>
+          <div className='wrapper'>
+            <img src={img6} alt="" />
+            <div className='information'>
+              <p>Kelly Saldaña</p>
+              <span> 10+ new 22h</span>
+            </div>
+          </div>
          <div className='icono'>
-           <img className='camara' src= {camara} alt="" />  
+           <img className='camara' src= {camara2} alt="" />  
          </div>
         </div>
         <div className='columna9'>
-          <img src={img5} alt="" />
-          <div className='information'>  
-            <p>Milena Diaz</p>
-            <span> seen 9h ago</span>
-         </div>
+          <div className='wrapper'>
+            <img src={img5} alt="" />
+            <div className='information'>
+              <p>Milena Diaz</p>
+              <span> seen 9h ago</span>
+            </div>
+          </div>
          <div className='icono'>
-           <img className='camara' src= {camara} alt="" />  
+           <img className='camara' src= {camara2} alt="" />  
          </div>
         </div>
         <div className='columna10'>
-          <img src={img4} alt="" />
-          <div className='information'>  
-            <p>Camila Carmona</p>
-            <span> 4 new messages 7h</span>
-         </div>
+          <div className='wrapper'>
+            <img src={img4} alt="" />
+            <div className='information'>
+              <p>Camila Carmona</p>
+              <span> 4 new messages 7h</span>
+            </div>
+          </div>
          <div className='icono'>
-           <img className='camara' src= {camara} alt="" />  
+           <img className='camara' src= {camara2} alt="" />  
          </div>
         </div>
         <div className='columna11'>
-          <img src={img3} alt="" />
-          <div className='information'>  
-            <p>Linda Montoya</p>
-            <span> 7 new messages 3h</span>
-         </div>
+          <div className='wrapper'>
+            <img src={img3} alt="" />
+            <div className='information'>
+              <p>Linda Montoya</p>
+              <span> 7 new messages 3h</span>
+            </div>
+          </div>
          <div className='icono'>
-           <img className='camara' src= {camara} alt="" />  
+           <img className='camara' src= {camara2} alt="" />  
          </div>
         </div>
-       
-
-
-
-
-
-
-
-
+        <div className='columna1'>
+          <div className='wrapper'>
+            <img src={img13} alt="" />
+            <div className='information'>
+              <p>Amanda Serna Leon</p>
+              <span> 10 new messages - 5h</span>
+            </div>
+          </div>
+         <div className='icono'>
+           <img className='camara' src= {camara2} alt="" />  
+         </div>
+        </div>
+        <div className='columna2'>
+          <div className='wrapper'>
+            <img src={img12} alt="" />
+            <div className='information'>
+              <p>Sofia Olarte</p>
+              <span> 4 new messages - 18h</span>
+                     </div>
+          </div>
+         <div className='icono'>
+           <img className='camara' src= {camara2} alt="" />  
+         </div>
+        </div>
       </div>
- 
-
-  
-
-
-
-
-
-
-
-
-
-
 
     </div>
    )
